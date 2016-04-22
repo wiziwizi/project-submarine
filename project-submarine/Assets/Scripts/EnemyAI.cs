dd43da00e734114365cd39da1fc7c1fd8784b2ff
Rico Evers <ricoevers@live.nl>
2016-04-22 14:11:37 +0200

    Enemy AI Setup
    enimy ai script begin gemaakt, werkt nu op de simpelste manier.

--- project-submarine/Assets/Scripts/EnemyAI.cs
+++ project-submarine/Assets/Scripts/EnemyAI.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
 
+	//private NavMeshAgent _navMeshAgent;
+	private GameObject _player;
+	//private int waypointIndex = 0;
+
+	//public Transform[] waypoints; 
+	public  float MoveSpeed;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () 
+	{
+		//_navMeshAgent = GetComponent<NavMeshAgent> ();
+		_player = GameObject.FindGameObjectWithTag ("Player");
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void  Update()
+	{
+		transform.LookAt (_player.transform.position);
+		transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 	}
 }
