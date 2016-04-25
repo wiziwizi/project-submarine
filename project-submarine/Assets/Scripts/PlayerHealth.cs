c5c91605d5f9fe951c7f0d69006bf4a3f24b3606
Rico Evers <ricoevers@live.nl>
2016-04-25 08:54:59 +0200

    init setup
    project setup

---
+++ project-submarine/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public float health = 100f;
+
+	private bool playerDead;
+
+	void Update()
+	{
+		if (health <= 0) 
+		{
+			Debug.Log ("hoi");
+
+			LevelReset ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Enemy"))
+		{
+			Debug.Log ("hoi");
+			health -= 22;
+		}
+	}
+
+	void LevelReset()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+
+}
