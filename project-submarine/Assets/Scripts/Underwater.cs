573a1a34d038541b6131e4fc3f4dfe71bf06d465
Rico Evers <ricoevers@live.nl>
2016-05-09 12:14:17 +0200

    Library deleted en git ignore gefixt.

---
+++ project-submarine/Assets/Scripts/Underwater.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Underwater : MonoBehaviour {
+
+	//This script enables underwater effects. Attach to main camera.
+
+	//Define variable
+	public int underwaterLevel = 7;
+
+	//The scene's default fog settings
+	private bool defaultFog = RenderSettings.fog;
+	private Color defaultFogColor = RenderSettings.fogColor;
+	private float defaultFogDensity = RenderSettings.fogDensity;
+	private Material defaultSkybox = RenderSettings.skybox;
+	private Material noSkybox;
+
+	void Start () {
+		//Set the background color
+		GetComponent<Camera>().backgroundColor = new Color(0, 0.4f, 0.8f, 1);
+	}
+
+	void Update () {
+		if (transform.position.y < underwaterLevel)
+		{
+			RenderSettings.fog = true;
+			RenderSettings.fogColor = new Color(0, 0.4f, 0.7f, 0.6f);
+			RenderSettings.fogDensity = 0.02f;
+			RenderSettings.skybox = noSkybox;
+		}
+		else
+		{
+			RenderSettings.fog = defaultFog;
+			RenderSettings.fogColor = defaultFogColor;
+			RenderSettings.fogDensity = defaultFogDensity;
+			RenderSettings.skybox = defaultSkybox;
+		}
+	}
+}
