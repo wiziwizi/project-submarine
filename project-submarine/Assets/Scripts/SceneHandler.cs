d771850653b856f3e67ed80a0543d0b7cd0a0cde
Rico Evers <ricoevers@live.nl>
2016-05-20 09:10:53 +0200

    scene handeler script #1

---
+++ project-submarine/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class SceneHandler : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Canvas optionMenu;
+	public Button startText;
+	public Button exitText;
+
+	void Start()
+	{
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		optionMenu = optionMenu.GetComponent<Canvas> ();
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+
+		quitMenu.enabled = false;
+		optionMenu.enabled = false;
+
+	}
+	public void NewGame()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void OptionMenu()
+	{
+		optionMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void QuitMenu()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void ExitQuitMenu()
+	{
+		optionMenu.enabled = false;
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void Menu()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+}
